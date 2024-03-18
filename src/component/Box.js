3f39d2a0e2bc3ebf972e2a56c4860a0538056302
김세현 <allezvvell@gmail.com>
2024-03-18 22:01:47 +0900

    2nd commit

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,7 @@ const Box = (props) => {
   return (
     <div className='box'>
        <h1 className='title'>{props.title}</h1>
-       <span className='img-box'><img src={props.item && props.item.img}/></span>
+       <span className='img-box'><img src={props.item && props.item.img} alt='가위바위보이미지'/></span>
        <h2 className='result'>{props.result}</h2>
     </div>
   )
